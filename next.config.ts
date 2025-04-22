b4304181ca214928c4eb41f9068d8314da7e4ed6
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2025-04-22 22:28:33 +0200

    app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["your-project-id.supabase.co"], 
+  },
 };
+
 
 export default nextConfig;
